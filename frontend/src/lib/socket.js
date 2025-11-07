7534680d2ea5b6e3ccd50f65bf812a1bb4dee932
TuanAP <tuanphamu23@gmail.com>
2025-11-07 21:23:11 +0700

    refactor(socket): replace accessToken retrieval from Redux with localStorage for socket connection

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,29 +1,28 @@
 import { io } from "socket.io-client";
-import { useSelector } from "react-redux";
 
 // URL backend
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:8383";
 
 // Lấy accessToken
-const { accessToken } = useSelector((state) => state.auth);
+const token = localStorage.getItem("accessToken");
 
 // Tạo instance socket
 const socket = io(SOCKET_URL, {
-  autoConnect: false, // Đợi login mới kết nối
-  transports: ["websocket"], // Dùng websocket để giảm độ trễ
-  auth: { accessToken }, // Gửi kèm token trong handShake
-  reconnectionAttempts: 5, // Nếu mất kết nối thử lại 5 lần
-  reconnectionDelay: 1000, // Delay 1s mỗi lần reconnect
+    autoConnect: false, // Đợi login mới kết nối
+    transports: ["websocket"], // Dùng websocket để giảm độ trễ
+    auth: { token }, // Gửi kèm token trong handShake
+    reconnectionAttempts: 5, // Nếu mất kết nối thử lại 5 lần
+    reconnectionDelay: 1000, // Delay 1s mỗi lần reconnect
 });
 
 // Hàm tiện ích để quản lý kêt nối
-export const connectSocket = (accessToken) => {
-  socket.auth = { accessToken };
-  socket.connect();
+export const connectSocket = (token) => {
+    socket.auth = { token };
+    socket.connect();
 };
 
 export const disconnectSocket = () => {
-  socket.disconnect();
+    socket.disconnect();
 };
 
 export default socket;
