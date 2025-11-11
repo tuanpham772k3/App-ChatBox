007beafcd93902c4d8cb5ba279ceeac62a086cb6
TuanAP <tuanphamu23@gmail.com>
2025-11-11 16:22:54 +0700

    feat(MessageInput): Implement message sending functionality

--- frontend/src/features/chat/components/MessageInput.jsx
+++ frontend/src/features/chat/components/MessageInput.jsx
@@ -1,7 +1,36 @@
+import React, { useState } from "react";
 import { Gift, Image, Mic, Smile, Sticker, ThumbsUp } from "lucide-react";
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createNewMessage } from "../messagesSlice";
 
 const MessageInput = () => {
+  const [text, setText] = useState("");
+  
+  const { user } = useSelector((state) => state.auth);
+  const { currentConversation } = useSelector((state) => state.conversations);
+
+  const dispatch = useDispatch();
+
+  // Xử lý gửi tin nhắn
+  const handleSend = async () => {
+    if (!text.trim()) return;
+
+    try {
+      await dispatch(
+        createNewMessage({
+          conversationId: currentConversation._id,
+          senderId: user.id,
+          content: text,
+          type: "text",
+        })
+      ).unwrap();
+
+      setText("");
+    } catch (error) {
+      console.log("Lỗi gửi tin nhắn:", error);
+    }
+  };
+
   return (
     <div className="flex items-center gap-3 px-2 py-3 border-t border-[var(--color-border)]">
       {/* Action btn */}
@@ -27,6 +56,9 @@ const MessageInput = () => {
       {/* Input Message */}
       <div className="flex-1 flex items-center justify-between bg-[var(--bg-gray)] rounded-full">
         <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleSend()}
           type="text"
           placeholder="Aa"
           className="flex-1 bg-[var(--bg-gray)] rounded-full ps-3 text-[var(--color-text-primary)] placeholder:text-[var(--color-text-secondary)] focus:outline-none"
