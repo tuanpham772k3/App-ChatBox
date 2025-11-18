b936108987ce41b90954cfd10f75fdef410035bd
TuanAP <tuanphamu23@gmail.com>
2025-11-18 17:20:13 +0700

    Move Sidebar component to layout/

--- frontend/src/pages/Chat/ChatPage.jsx
+++ frontend/src/pages/Chat/ChatPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "@/components/layout/Sidebar";
 import ChatWindow from "@/features/chat/components/ChatWindow";
 import ConversationContainer from "@/features/conversations/ConversationsContainer";
 
