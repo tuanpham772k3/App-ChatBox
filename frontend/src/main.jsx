775ea250c0b0de62ed2071074a3507970ab4aa01
TuanAP <tuanphamu23@gmail.com>
2026-04-11 22:23:40 +0700

    refactor: remove redux-persist integration and related code from store and main components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,27 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../src/styles/globals.css";
 import App from "./App.jsx";
-import { persistor, store } from "../src/store/store";
+import { store } from "../src/store/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
 import { ConfigProvider } from "antd";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <ConfigProvider
-            theme={{
-              token: {
-                fontFamily: "var(--my-font)",
-              },
-            }}
-          >
-            <App />
-          </ConfigProvider>
-        </PersistGate>
+        <ConfigProvider
+          theme={{
+            token: {
+              fontFamily: "var(--my-font)",
+            },
+          }}
+        >
+          <App />
+        </ConfigProvider>
       </Provider>
     </BrowserRouter>
   </StrictMode>
